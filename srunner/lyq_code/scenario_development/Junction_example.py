a011e898a5b72f6753875d2d54260d1961e36811
lyq <550195458@qq.com>
2020-06-25 00:55:00 +0800

    Adding example of junction scenario env.

---
+++ srunner/lyq_code/scenario_development/Junction_example.py
@@ -0,0 +1,56 @@
+"""
+This is an example of junction scenario.
+right turn with a straight npc vehicle.
+"""
+
+from __future__ import print_function
+import glob
+import os
+import sys
+
+# carla 098 API is recommended
+sys.path.append("/home/lyq/CARLA_simulator/CARLA_098/PythonAPI/carla")
+sys.path.append("/home/lyq/CARLA_simulator/CARLA_098/PythonAPI/carla/agents")
+carla_path = '/home/lyq/CARLA_simulator/CARLA_098/PythonAPI'
+
+try:
+    sys.path.append(glob.glob(carla_path + '/carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+import time
+import random
+import numpy as np
+import math
+import traceback
+
+from srunner.lyq_code.env.BasicEnv import BasicEnv
+
+
+class Env(BasicEnv):
+    def __init__(self):
+        """
+
+        """
+
+    def run(self):
+        """"""
+
+        print('d')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.run()
+
+    print('done')
